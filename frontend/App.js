6d66bebdfcf72d0258893b970eeb23730c7b44a3
QuinHouck <quintenhouck@gmail.com>
2025-01-07 19:13:34 -0500

    feat: changed name and slug, added nfl

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,6 @@
+import 'react-native-gesture-handler';  // Ensure this is at the top of the file
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -18,19 +21,22 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{gestureEnabled: false, title: 'Screens'}}>
-        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Store" component={StoreScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="QuizOption" component={QuizOptionScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="QuizGame" component={QuizGameScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="QuizResults" component={QuizResultsScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="TestOption" component={TestOptionScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="TestGame" component={TestGameScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Acknowledgements" component={Acknowledgements} />
-      </Stack.Navigator>
-      <StatusBar style="light" />
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{gestureEnabled: false, title: 'Screens'}}>
+          <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="Store" component={StoreScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="QuizOption" component={QuizOptionScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="QuizGame" component={QuizGameScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="QuizResults" component={QuizResultsScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="TestOption" component={TestOptionScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="TestGame" component={TestGameScreen} />
+          <Stack.Screen options={{ headerShown: false }} name="Acknowledgements" component={Acknowledgements} />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </GestureHandlerRootView>
+    
   );
 }
 
--- frontend/src/util/getImages.js
+++ frontend/src/util/getImages.js
@@ -12,6 +12,8 @@ export async function getImages(chosen, pack){
             return await getFlags(chosen);
         case "presidents":
             return await getPresidents(chosen);
+        case "nfl":
+            return await getNFL(chosen);
     }
 }
 
@@ -77,4 +79,36 @@ async function getPresidents(chosen){
     };
 
     return {imgs: obj, height: '70%'};
+}
+
+async function getNFL(chosen){
+    const url = `../images/sports/nfl`;
+    const images = importAll(require.context(url, false, /\.(png|jpe?g|svg)$/));
+
+    function getInfo(def){
+        return new Promise((resolve, reject) => {
+            Image.getSize(def, (width, height) => {
+                resolve({width, height});
+            });
+        }, (error) => {
+            reject(error);
+        })
+    }
+
+    const obj = {};
+    for(const team of chosen) {
+        const key = `${team["name"]}.png`;
+        const def = Image.resolveAssetSource(images[key]).uri;
+        const {width, height} = await getInfo(def);
+
+        const ar = width/height;
+        const data = {
+            image: images[key],
+            uri: def,
+            ar: ar,
+        };
+        obj[team.name] = data;
+    };
+
+    return {imgs: obj, height: '60%'};
 }
