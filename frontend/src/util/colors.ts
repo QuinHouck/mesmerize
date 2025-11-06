ea0d66c9534aaeedd7dd418b05f8d56becac2b1b
QuinHouck <quintenhouck@gmail.com>
2025-11-05 23:53:00 -0500

    feat: updated node to 20.19.4 to fit sdk 54. upgraded colors to typescript

--- frontend/src/util/colors.ts
+++ frontend/src/util/colors.ts
@@ -3,10 +3,21 @@
 // export const lightPurple = '#745e96';
 // export const darkPurple = '#3b2c5e';
 
-export default colors ={
+interface Colors {
+    whitePurple: string;
+    lightPurple: string;
+    lightPurpleShadow: string;
+    darkPurple: string;
+    darkGrey: string;
+}
+
+const colors: Colors = {
     whitePurple: '#e5e0f0',
     lightPurple: '#745e96',
     lightPurpleShadow: '#52406e',
     darkPurple: '#3b2c5e',
     darkGrey: '#222222'
-}
+};
+
+export default colors;
+
